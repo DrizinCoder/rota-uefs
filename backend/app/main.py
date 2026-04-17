55463ddd48ef0bb60acda07462226bef25d41c8e
Robson-Carvalho <rcsouza@ecomp.uefs.br>
2026-04-17 00:04:36 -0300

    feat: adiciona endpoints de motorista, estudante e administradores
    Co-authored-by: Marcelosgc1 <marcelobtsgc1@gmail.com>

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.routers.routes import router
 
 app = FastAPI()
 
 @app.get("/")
 def health_check():
-    return {"message": "Rota UEFS Backend is running"}
+    return {"message": "Rota UEFS Backend is r"}
+
+app.include_router(router)
