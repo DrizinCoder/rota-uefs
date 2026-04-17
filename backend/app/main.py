b418774b3a14099d58df5452b4c4e3225ee9a2b3
Guilherme Fernandes <guiaulas2020@gmail.com>
2026-04-17 00:37:22 -0300

    feat: Cria User Repository

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,8 +3,8 @@ from app.routers.routes import router
 
 app = FastAPI()
 
+app.include_router(router)
+
 @app.get("/")
 def health_check():
     return {"message": "Rota UEFS Backend is r"}
-
-app.include_router(router)
---
+++ backend/app/repositories/user_repository.py
@@ -0,0 +1,54 @@
+import uuid
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from app.database.models import User
+
+class UserRepository:
+
+    def __init__(self, session: AsyncSession):
+        self.session = session
+    
+    async def list_all(self):
+        statement = select(User)
+        result = await self.session.execute(statement)
+        return result.scalars().all()
+    
+    async def get_by_id(self, user_id: uuid.UUID):
+        statement = select(User).where(User.user_id == user_id)
+        result = await self.session.execute(statement)
+        return result.scalars().first()
+
+    async def get_by_registration_id(self, user_id: uuid.UUID):
+        statement = select(User).where(User.registration_id == user_id)
+        result = await self.session.execute(statement)
+        return result.scalars().first()
+    
+    async def create(self, user: User):
+        self.session.add(user)
+        await self.session.commit()
+        await self.session.refresh(user)
+        return user
+    
+    async def update(self, user: User):
+        self.session.add(user)
+        await self.session.commit()
+        await self.session.refresh(user)
+        return user
+    
+    async def patch(self, user_id: int, update_data: dict):
+        db_user = await self.get_by_id(user_id)
+        if not db_user:
+            return None
+
+        for key, value in update_data.items():
+            setattr(db_user, key, value)
+
+        self.session.add(db_user)
+        await self.session.commit()
+        await self.session.refresh(db_user)
+        return db_user
+
+    async def delete(self, user: User):
+        await self.session.delete(user)
+        await self.session.commit()
+        return user
