e4030225a02df57d0c3da6e7565a808db680f184
Robson-Carvalho <rcsouza@ecomp.uefs.br>
2026-04-19 00:20:16 -0300

    refactor: remove a criação de inúmeras sessionmaker e facilita conexão com BD

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -1,18 +1,36 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+import asyncio, logging
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.asyncio.session import AsyncSession
-from sqlmodel import SQLModel
+from typing import AsyncGenerator
 from app.core.config import settings
+logger = logging.getLogger("uvicorn")
 
-engine = create_async_engine(settings.DATABASE_URL, echo=False, future=True)
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=False,
+    future=True
+)
 
-async def get_session() -> AsyncSession:
-    async_session = sessionmaker(
-        engine, class_=AsyncSession, expire_on_commit=False
-    )
-    async with async_session() as session:
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
         yield session
 
 async def init_db():
-    async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.create_all)
+    for i in range(10):
+        try:
+            async with engine.begin() as conn:
+                logger.info("✅ Banco conectado...")
+                
+                return
+        except Exception as e:
+            logger.warning(f"⏳ Tentando conectar ao banco... ({i+1}/10)")
+            await asyncio.sleep(2)
+
+    raise Exception("❌ Não conseguiu conectar ao banco")
