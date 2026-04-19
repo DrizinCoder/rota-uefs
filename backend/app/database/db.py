fe4b237fc4aae2947e4ab0e1fe41b9261fb59a5d
Robson-Carvalho <rcsouza@ecomp.uefs.br>
2026-04-19 01:06:51 -0300

    feat: adiciona sync do ORM com o BD

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -3,6 +3,8 @@ import asyncio, logging
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from typing import AsyncGenerator
+
+from sqlmodel import SQLModel
 from app.core.config import settings
 logger = logging.getLogger("uvicorn")
 
@@ -26,8 +28,8 @@ async def init_db():
     for i in range(10):
         try:
             async with engine.begin() as conn:
+                await conn.run_sync(SQLModel.metadata.create_all)
                 logger.info("✅ Banco conectado...")
-                
                 return
         except Exception as e:
             logger.warning(f"⏳ Tentando conectar ao banco... ({i+1}/10)")
