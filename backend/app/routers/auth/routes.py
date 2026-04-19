97859b9546d937f16af1127da6612cf593152adb
Guilherme Fernandes <guiaulas2020@gmail.com>
2026-04-19 02:43:48 -0300

    feat: Implementa fluxo de cadastro aluno

--- backend/app/routers/auth/routes.py
+++ backend/app/routers/auth/routes.py
@@ -1,3 +1,9 @@
+from app.core.responses import ResponseHandler
+from app.core.exceptions import ConflictException
+from app.database.db import get_session
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.repositories.user_repository import UserRepository
 from app.DTOs.auth.dtos import RegisterAlunoDTO
 from fastapi import APIRouter
 from app.DTOs.auth.dtos import RegisterServidorDTO, RegisterUserDTO, LoginUserDTO
@@ -13,8 +19,17 @@ async def register_motorista(dados: RegisterUserDTO):
     return {"message": "olá! bem-vindo a registro de motorista"}
 
 @router.post("/register/aluno")
-async def register_aluno(dados: RegisterAlunoDTO):
-    return {"message": "olá! bem-vindo a registro de alunos"}
+async def register_aluno(dados: RegisterAlunoDTO, session: AsyncSession = Depends(get_session)):
+    repo = UserRepository(session)
+
+    student = repo.find_by_email(dados.email)
+    
+    if student:
+        raise ConflictException("Usuário já cadastrado")
+
+    student_created = repo.create_user(dados)
+
+    return ResponseHandler.created(student_created)
 
 @router.post("/register/admin")
 async def register_administrador(dados: RegisterUserDTO):
