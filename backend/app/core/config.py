163ac3a3fb94cc1c3367d700c89cc2661247469a
joaom <joaomarcelo642@gmail.com>
2026-04-20 14:15:06 -0300

    feat: configuraçao de variaveis de ambiente

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,14 +2,29 @@ import logging
 import os
 
 from dotenv import load_dotenv
+from pydantic import BaseSettings
 
 load_dotenv()
 
-class Settings:
+class Settings(BaseSettings):
     DATABASE_URL: str = os.getenv("DATABASE_URL")
     SECRET_KEY: str = os.getenv("SECRET_KEY", "sua-chave-secreta-aqui-mude-em-producao")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "360"))
+
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: int = 587
+    MAIL_SERVER: str = "smtp.gmail.com"
+    MAIL_FROM_NAME: str = "Rota UEFS"
+    MAIL_STARTTLS: bool = True
+    MAIL_SSL_TLS: bool = False
+    USE_CREDENTIALS: bool = True
+    VALIDATE_CERTS: bool = True
+
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
 
@@ -23,3 +38,4 @@ def setup_logging():
     root_logger.addHandler(console_handler)
     
     root_logger.setLevel(logging.INFO)
+
