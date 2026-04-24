74385af70d3d400677a841aa5cac23172d70efe7
oLeozito <leonardooliver222@hotmail.com>
2026-04-23 23:58:31 -0300

    feat: cria pasta services, adiciona API e inicia authService

---
+++ frontend/services/authService.ts
@@ -0,0 +1,39 @@
+import { api } from "./api";
+
+// 1. DTO de Cadastro do Aluno
+export interface RegisterAlunoDTO {
+  full_name: string;
+  password: string;
+  registration_id: string; // É a matrícula
+  phone: string;
+  email: string; // Ex: 12345678@discente.uefs.br
+}
+
+// 2. DTO de Cadastro do Servidor
+export interface RegisterServidorDTO {
+  full_name: string;
+  password: string;
+  registration_id: string;
+  phone: string;
+  email: string;
+  department: string;
+  employment: string; 
+}
+
+// 3. DTO de Login
+export interface LoginUserDTO {
+  matricula: string;
+  senha: string;
+}
+
+export const authService = {
+    login: async (dados: LoginUserDTO) => {
+        const response = await api.post('/auth/login', dados);
+        return response.data;
+        },
+    cadastroAluno: async (dados: RegisterAlunoDTO) => {
+        const response = await api.post('/auth/register/aluno', dados);
+        return response.data;
+    },
+    
+};
