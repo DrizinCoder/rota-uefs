c593c2c0c41c80d71c30bb388f7c57db4c0bd9af
oLeozito <leonardooliver222@hotmail.com>
2026-04-24 22:57:37 -0300

    fix: ajusta arquivos next.config.mjs para corrigir build/subida no Docker

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+  
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
   },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
-export default nextConfig
+export default nextConfig;
