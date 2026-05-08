7c11b5f2c831fcdc61afecad8d73526e07a92426
Guilherme Fernandes <guiaulas2020@gmail.com>
2026-05-08 00:44:40 -0300

    feat(utils): Cria função geradora e hmac

--- backend/app/utils/utils.py
+++ backend/app/utils/utils.py
@@ -1,6 +1,21 @@
 import time
 from datetime import datetime, timedelta
+import uuid
+import hmac
+import hashlib
+
+from app.core.config import settings
 
 def add_ninety_minutes(t: time) -> time:
     dummy = datetime(2000, 1, 1, t.hour, t.minute, t.second)
     return (dummy + timedelta(minutes=90)).time()
+
+def generate_registration_code(
+    reservation_id: uuid.UUID,
+    trip_id: uuid.UUID,
+    registration_id: str,
+) -> str:
+    secret = settings.REGISTRATION_CODE_SECRET.encode()
+    message = f"{reservation_id}:{trip_id}:{registration_id}".encode()
+    digest = hmac.new(secret, message, hashlib.sha256).hexdigest()
+    return f"{reservation_id}.{digest}"
