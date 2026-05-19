1ea06777d09950f3c500abfb663e96cb1e7fa0a2
joaom <joaomarcelo642@gmail.com>
2026-05-19 15:30:44 -0300

    feat: Criação dos DTOs do relatório

---
+++ backend/app/DTOs/reports.py
@@ -0,0 +1,20 @@
+import uuid
+from datetime import date, time
+from pydantic import BaseModel
+from typing import List
+
+class PassengerInsuranceItem(BaseModel):
+    name: str
+    email: str
+    registration_id: str
+    user_role: str
+
+class TripInsuranceItem(BaseModel):
+    trip_id: uuid.UUID
+    trip_date: date
+    departure_time: time
+    bus_license_plate: str
+    boarding_point: str
+    drop_off_point: str
+    total_passengers: int
+    passengers: List[PassengerInsuranceItem]
